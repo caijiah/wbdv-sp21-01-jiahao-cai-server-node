68f251d28aa0f12f4d33b9f598906f080b405b86
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-05 20:53:48 -0400

    implement questions service

---
+++ services/questions/questions-service.js
@@ -0,0 +1,26 @@
+const questions = require('./questions.json')
+
+const createQuestion = () => {}
+const createQuestionForQuiz = () => {}
+
+const findAllQuestions = () => {
+    return questions
+}
+
+const findQuestionsForQuiz = (qzid) => {
+    return questions.filter(question => question.quizId === qzid)
+}
+
+const findQuestionById = (quid) => {
+    return questions.find(question => question._id === quid)
+}
+
+const updateQuestion = () => {}
+const deleteQuestion = () => {}
+
+module.exports = {
+    createQuestion, findAllQuestions,
+    findQuestionById, updateQuestion,
+    deleteQuestion, createQuestionForQuiz,
+    findQuestionsForQuiz
+}
