8954b0af95d12fe71a203cacf8ec9a4440c31f87
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-04-13 21:35:39 -0400

    make controller use new services

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -3,13 +3,30 @@ const questionsService = require("../services/questions/questions-service")
 module.exports = (app) => {
     const findQuestionsForQuiz = (req, res) => {
         const qzid = req.params['qzid']
-        res.send(questionsService.findQuestionsForQuiz(qzid))
+        // res.send(questionsService.findQuestionsForQuiz(qzid))
+        questionsService.findQuestionsForQuiz(qzid)
+            .then((questions) => {
+                res.send(questions)
+            })
     }
 
     const findAllQuestions = (req, res) => {
-        res.send(questionsService.findAllQuestions())
+        questionsService.findAllQuestions()
+            .then((questions) => {
+                res.send(questions)
+            })
+    }
+
+    const findQuestionById = (req, res) => {
+        const quid = req.params['quid']
+        questionsService.findQuestionById(quid)
+            .then(question => {
+                res.send(question)
+            })
     }
 
     app.get("/api/quizzes/:qzid/questions", findQuestionsForQuiz)
     app.get("/api/questions", findAllQuestions)
+    app.get("/api/questions/:quid", findQuestionById)
+
 }
